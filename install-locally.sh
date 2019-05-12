7042596a7edf07132b860ae85f3efe7176af1260
Ashraf Hadden <ashrafhadden@gmail.com>
2019-05-12 19:44:52 -0400

    rm install-locally.sh

--- install-locally.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Bypass "ERROR Invalid extension name 'dracula.min'" during `vsce package`
-# due to the "." in "dracula.min" in package.json name
-# Also much faster than vsce package
-
-rsync package.json ~/.vscode/extensions/ashrafhadden.dracula.min-1.3.0
-rsync -r themes ~/.vscode/extensions/ashrafhadden.dracula.min-1.3.0
-rsync icon.png ~/.vscode/extensions/ashrafhadden.dracula.min-1.3.0
